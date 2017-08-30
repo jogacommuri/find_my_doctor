99e8b56c98ae5587af2aa0324f3988a1b2386479
Joga Commuri <jogacommuri@yahoo.com>
2017-08-30 00:38:36 -0700

    pod design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 //import { render } from 'react-dom';
+import { Well, Col, Row} from 'react-bootstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
@@ -7,7 +8,7 @@ import TodoForm from './components/todoForm'
 import TodoList from './components/todoList'
 import Message from './components/Message'
 //import {bindActionCreators} from 'redux'
-import Footer from './components/Footer'
+import Filter from './components/Filter'
 
 
 class App extends Component {
@@ -16,17 +17,23 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2> Todo App </h2>
+          <h2> Find My Doctor App </h2>
         </div>
         <Router>
-          <div className="Todo-App">
-            <Message />
-            <TodoForm />
-            <Route path='/:filter?' render={({match}) => (
-                <TodoList filter={match.params.filter} />
-              )} />
-            <Footer />
-          </div>
+          <Well>
+            <Row>
+              <Col xs={12} sm={12} md={10} mdOffset={2} lg={12} lgOffset={2}>
+                <div className="Todo-App">
+                  <Message />
+                  <TodoForm />
+                  <Filter />
+                  <Route path='/:filter?' render={({match}) => (
+                      <TodoList filter={match.params.filter} />
+                    )} />
+                </div>
+              </Col>
+            </Row>
+          </Well>
         </Router>
       </div>
     );
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Image, Col} from 'react-bootstrap';
 import {fetchTodos, toggleTodo, deleteTodo, getVisibleTodos} from '../reducers/todo'
+import Rater from 'react-rater';
+import Male from '../assets/male_doctor.png';
+import Female from '../assets/female_doctor.jpg';
+import 'react-rater/lib/react-rater.css';
 
-
-const TodoItem = ({id,name,isComplete, toggleTodo, deleteTodo})=>(
-	<li>
-		<span className="delete-item">
-			<button onClick={() => deleteTodo(id)}> X </button>
-		</span>
-		<input type ="checkbox" 
-			checked={isComplete} 
-			onChange={() => toggleTodo(id)}/> 
-		{name}
+const TodoItem = ({id, name, area, img, specialty, isComplete, contactNo, description, review, gender,toggleTodo, deleteTodo})=>(
+	<li> 
+		<Col xs={12} sm={12} md={4} lg={4} className="smallPod">
+			<span className="delete-item">
+				<button onClick={() => deleteTodo(id)}> X </button>
+			</span>
+			<input type ="checkbox" 
+				checked={isComplete} 
+				onChange={() => toggleTodo(id)}/> 
+			<Col xs={4} sm={4} md={4} lg={4}>
+				{({gender}.gender === "Male") ? <Image src={Male} rounded/> : <Image src={Female} rounded/>}
+				
+			</Col>
+			<Col xs={8}  sm={8} md={8} lg={8}>
+				<span>
+					<p>{(isComplete) ? "Accepting New Patients" : "Not Accepting new Patients"}</p>
+					<p>Name: {name}</p>
+					<p>Area: {area}</p>
+					<p>Specialty: {specialty}</p>
+					<p>{gender}</p>
+					<a>{contactNo}</a>
+				</span>
+				<br/>
+				Rating: ({review})<Rater interactive={false} rating={review} />
+			</Col>
+			<Col xs={12} sm={12} md={12} lg={12} className="description">
+				<span>
+					<p>About Me: </p>
+					<p>{description}</p>
+				</span>
+				
+			</Col>
+		</Col>
 	</li>
+	
 )
 class TodoList extends Component{
 	componentDidMount(){
